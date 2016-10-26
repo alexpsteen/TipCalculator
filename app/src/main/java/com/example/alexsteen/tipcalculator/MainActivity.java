6ad818fecbbcbadc1c30583825539db435289dfb
alexpsteen <alexpsteen@gmail.com>
2016-10-26 00:41:55 -0400

    Created Calc Activity

--- app/src/main/java/com/example/alexsteen/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/alexsteen/tipcalculator/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.alexsteen.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * @Author: Alex Steen
@@ -12,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button enter = (Button) findViewById(R.id.screenBtn);
+
+
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CalcActivity.class));
+            }
+        });
     }
 }
