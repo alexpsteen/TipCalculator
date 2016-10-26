4e854b25cb3efa45a70985417efb215335fc27bc
alexpsteen <alexpsteen@gmail.com>
2016-10-26 17:00:14 -0400

    Created and Edited Receipt Screen

--- app/src/main/java/com/example/alexsteen/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/alexsteen/tipcalculator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.alexsteen.tipcalculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
