6f7335b851ef5a46636c98c2af81c37c0d09ce65
alexpsteen <alexpsteen@gmail.com>
2016-11-07 15:16:43 -0500

    Adding Radio Button Functionality

--- app/src/main/java/com/example/alexsteen/tipcalculator/CalcActivity.java
+++ app/src/main/java/com/example/alexsteen/tipcalculator/CalcActivity.java
@@ -79,21 +79,8 @@ public class CalcActivity extends AppCompatActivity {
             public void afterTextChanged(Editable s) {}
         });
 
-
         tip = 15.0;
 
-//        p12.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                tip = 12;recalculate();
-//            }
-//        });
-//        p15.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                tip = 15;recalculate();
-//            }
-//        });
         p12.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -122,28 +109,18 @@ public class CalcActivity extends AppCompatActivity {
                 return false;
             }
         });
-
-//        p12.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {tax = 12;recalculate();}});
-//        p15.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {tax = 15;recalculate();}});
-//        p18.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {tax = 18;recalculate();}});
-//        p20.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {tax = 20;recalculate();}});
-//        pCust.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {tax = 12;recalculate();}});
+        pCust.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                tip = 12;recalculate();
+                return false;
+            }
+        });
 
         recalculate();
     }
 
     public void onRBClick() {
-
     }
 
     public void recalculate() {
